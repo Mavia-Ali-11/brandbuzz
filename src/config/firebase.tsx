b24f02da5ee1069e7ecafab621004855190c562b
Mavia Ali <maviaali597@gmail.com>
2024-04-03 00:57:54 +0500

    Update firebase.tsx

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -9,14 +9,15 @@ import {
     signOut
 } from "firebase/auth";
 
+// Fill below firebaseConfig with Firebase valid API keys when working (removed due to github security alert)
 const firebaseConfig = {
-    apiKey: "AIzaSyCta9BCIUvxrHvzpS_ZZ_elJzbWHZ22XDs",
-    authDomain: "brandzbuzz-8429e.firebaseapp.com",
-    projectId: "brandzbuzz-8429e",
-    storageBucket: "brandzbuzz-8429e.appspot.com",
-    messagingSenderId: "11228078584",
-    appId: "1:11228078584:web:ed6c7edffbb26508cdf60d",
-    measurementId: "G-31VV2FPNWR"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
